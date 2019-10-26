a107a94662c6546399113bebe31eb0f87017d3fc
kostasA97 <kostasA97@gmail.com>
2019-10-26 18:49:11 +0300

    Find_traversal + Find_diagonal fixed (1st attempt)

--- lib/Traversals.h
+++ lib/Traversals.h
@@ -7,4 +7,5 @@ void show_grid_bhc_usage(string name);
 void shift_grid(vector<double>*, int, int);
 void hash_curve(vector<vector<double>>*, vector<double*>*, vector<double>*, double, int);
 void find_diagonal(vector<vector<int>>*, int, int);
+void find_traversals(vector<vector<int>>*, int, int, int, int, vector<vector<int>>*);
 void Relevant_Traversals(vector<vector<vector<int>>>*, int, int);
