f5fd82768ed740398dcaf8a812703d5994cab0de
Fanarosss <theof.aslanidis@gmail.com>
2019-11-11 17:16:58 +0200

    remove duplicates

--- lib/LSH.h
+++
@@ -1,5 +0,0 @@
-#include "Library.h"
-
-using namespace std;
-
-template <typename Point> void LSH (vector<vector<Point>>*, vector<vector<Point>>*, int, int, Point, double, vector<vector<int>>*, Point**, double**, int**);
