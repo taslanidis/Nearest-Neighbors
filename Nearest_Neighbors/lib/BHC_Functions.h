7fc864d874f79292160cc18f89d86a9d3667a068
Fanarosss <theof.aslanidis@gmail.com>
2019-11-19 12:33:45 +0200

    clean

--- Nearest_Neighbors/lib/BHC_Functions.h
+++
@@ -1,9 +0,0 @@
-#include "Library.h"
-
-using namespace std;
-
-void show_bhc_usage(string name);
-
-void fill_dictionary (map<int,int>* ,vector<vector<int>>);
-int calculate_vertex (vector<vector<int>>, map<int,int>, int);
-int hammingDistance(int, int);
